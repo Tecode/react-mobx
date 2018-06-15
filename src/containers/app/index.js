47763f4d4aaf87eda4be05320a3a32e03d8ee369
谢明 <ming.xie@socialcredits.cn>
2018-06-15 10:36:12 +0800

    feat: 增加了路由

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 import styles from './index.less';
 import NavBar from 'components/NavBar';
+import HomePage from '../homePage';
+import Article from '../article';
 // import Modal from 'components/common/Modal';
 // import Message from 'components/common/Message';
 
@@ -26,13 +29,14 @@ export default class App extends Component {
       <React.Fragment>
         <div className={styles.wrap} id="appWrap">
           {false && <DevTools />}
-          <NavBar/>
-          {/* <Modal modalStore={this.props.modalStore}/> */}
-          {/* <Message messageStore={this.props.messageStore}/> */}
-          <div className={styles.content}>
-            45
-          {/* {this.props.children} */}
-          </div>
+          <NavBar>
+            <Switch>
+              <Route exact path="/home" component={HomePage} />
+              <Route exact path="/article" component={Article} />
+              {/*<Route exact path="/users" name="home-users" component={NotFound} />*/}
+              <Redirect to="/article" />
+            </Switch>
+          </NavBar>
         </div>
       </React.Fragment>
     );
---
+++ src/containers/article/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+// import HomeBody from 'components/homePage';
+
+@observer
+export default class HomePage extends Component {
+  render() {
+    return (
+        <p>
+        文章
+        </p>
+    );
+  }
+}
